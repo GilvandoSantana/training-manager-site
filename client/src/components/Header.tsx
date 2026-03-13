2ecfa463746758a4c98c6f0d550611df7e27f5c0
Gilvando Santana <gilvando.santana@example.com>
2026-03-13 19:15:42 -0400

    feat: atualizar imagem de capa do site

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,6 +4,7 @@
  */
 
 import { Plus, Download, Shield, FileText } from 'lucide-react';
+import HERO_IMAGE from '../assets/hero-banner.webp';
 
 interface HeaderProps {
   onNewEmployee: () => void;
@@ -12,8 +13,6 @@ interface HeaderProps {
   isSyncing: boolean;
   employeeCount: number;
 }
-
-const HERO_IMAGE = 'https://private-us-east-1.manuscdn.com/sessionFile/0sxYxMlkBINnWNXBvTncJN/sandbox/GjMkiOLADQ2vFWSdbJgtvK-img-1_1771348556000_na1fn_aGVyby1iYW5uZXI.jpg?x-oss-process=image/resize,w_1920,h_1920/format,webp/quality,q_80&Expires=1798761600&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9wcml2YXRlLXVzLWVhc3QtMS5tYW51c2Nkbi5jb20vc2Vzc2lvbkZpbGUvMHN4WXhNbGtCSU5uV05YQnZUbmNKTi9zYW5kYm94L0dqTWtpT0xBRFEydkZXU2RiSmd0dkstaW1nLTFfMTc3MTM0ODU1NjAwMF9uYTFmbl9hR1Z5YnkxaVlXNXVaWEkuanBnP3gtb3NzLXByb2Nlc3M9aW1hZ2UvcmVzaXplLHdfMTkyMCxoXzE5MjAvZm9ybWF0LHdlYnAvcXVhbGl0eSxxXzgwIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzk4NzYxNjAwfX19XX0_&Key-Pair-Id=K2HSFNDJXOU9YS&Signature=e-BPZiMwj5eq4SfRADsAIH2sR1zY3DocWIco-kfjdltyLzLXztqW-PUDVYs63SrcEnJEaOtjZoLBSYe8FrrpZZDBbx4bABbITOoHWlO2jAdeKcKq1mBobJSdX8OsF5LbId6lpMIy3cEAXySccYx46Uvtatu0Un514z6WM-FgWh-ub9tsYkg5zf-otk3gKXhwo6ZKijEFbOZkn4zcXbDtnhgfEGQ1S~X6uFRNZHfWwpXn0VNqxqM1lMNWAfwSy7DeqNS4mm4an86TtKBvXjcFlOLuZy5i~CXV4YV9ICmLv77mgd5vtHvYHyYWky~F9dHNIHAxg2N7E1A5weQHlQ1FsA__';
 
 export default function Header({ onNewEmployee, onExport, onExportPDF, isSyncing, employeeCount }: HeaderProps) {
   return (
